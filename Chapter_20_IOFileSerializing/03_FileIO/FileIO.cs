e5ffd45f2ae10b6005e4c3820fc31e89a40a6799
Viacheslav <evteevne.ss@gmail.com>
2022-03-02 23:08:02 +0300

    Добавлен проект работы с классом File

---
+++ Chapter_20_IOFileSerializing/03_FileIO/FileIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _03_FileIO
+{
+    internal class FileIo
+    {
+        private static void Main()
+        {
+            Console.Title = "Работа с классом File";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("**************File test app**************\n");
+            var tasks = new [] {"Задача 1", "Задача 2", "Задача 3", "Задача 4" };
+            File.AppendAllLines(@".\Tasks.dat", tasks);
+            foreach (var line in File.ReadAllLines(@".\Tasks.dat"))
+            {
+                Console.WriteLine($"TODO: {line}");
+            }
+            
+            Console.WriteLine("\n**************Работа приложения завершена**************");
+            Console.ReadLine();
+        }
+    }
+}
