ff8252b497abcc450c6e3aae2607933027f12665
Viacheslav <evteevne.ss@gmail.com>
2022-03-21 21:56:58 +0300

    Изучение поставщиков данных часть 2

--- Chapter_21_ADO.Net/01_ConnectionFactory/ConnectionFactory.cs
+++ Chapter_21_ADO.Net/01_ConnectionFactory/ConnectionFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Data;
 using System.Data.Odbc;
 using System.Data.OleDb;
@@ -22,6 +23,19 @@ namespace _01_ConnectionFactory
             Console.WriteLine("************Simple Connection Factory************\n");
             var myConnection = GetConnection(DataProvider.SqlServer);
             Console.WriteLine($"Подключение: {myConnection.GetType().Name}");
+            var provider = ConfigurationManager.AppSettings["provider"];
+            if (Enum.IsDefined(typeof(DataProvider), provider))
+            {
+                var dataProvider = (DataProvider) Enum.Parse(typeof(DataProvider), provider);
+                myConnection = GetConnection(dataProvider);
+                Console.WriteLine($"Подключение: {myConnection.GetType().Name}");
+            }
+            else
+            {
+                Console.WriteLine("Неверно задан тип подключения в файле конфигурации");
+                Console.ReadLine();
+                return;
+            }
             Console.WriteLine("\n************Работа приложения завершена************");
             Console.ReadLine();
         }
