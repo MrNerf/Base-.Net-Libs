26be7b4230778c944e8f9bf08df043340b4cc3a0
Viacheslav <evteevne.ss@gmail.com>
2022-04-03 14:23:21 +0300

    DataProvider Factory часть 1

---
+++ Chapter_21_ADO.Net/03_AutoLotDataReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _03_AutoLotDataReader
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = "Понятие подключенного уровня ADO.Net";
+            Console.WriteLine("************Data Reader************\n");
+            using (var sql = new SqlConnection())
+            {
+                //Символ . указывает, что используется локальная машина
+                sql.ConnectionString = @"Data Source = .; Initial Catalog = AutoLot; Integrated Security = True; Connect Timeout = 30";
+                sql.Open();
+                var sqlCommand = new SqlCommand("select * from Inventory", sql);
+                using (var dataReader = sqlCommand.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine($"-> {dataReader["Mark"]}, " +
+                                          $"{dataReader["Color"]}, " +
+                                          $"{dataReader["PetName"]}");
+                    }
+                }
+            }
+            Console.WriteLine("\n************Работа приложения завершена************");
+            Console.ReadLine();
+        }
+    }
+}
