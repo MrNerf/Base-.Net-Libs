66ba444c455e4bcc9b27d582e8dc5a3c84e71af5
Viacheslav <evteevne.ss@gmail.com>
2022-04-03 14:34:17 +0300

    DataProvider Factory часть 2

--- Chapter_21_ADO.Net/03_AutoLotDataReader/AutoLotDataReader.cs
+++ Chapter_21_ADO.Net/03_AutoLotDataReader/AutoLotDataReader.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.Data.Common;
 using System.Data.SqlClient;
 
 namespace _03_AutoLotDataReader
 {
-    internal class Program
+    internal class AutoLotDataReader
     {
         private static void Main()
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Title = "Понятие подключенного уровня ADO.Net";
             Console.WriteLine("************Data Reader************\n");
+            var sqlConnectionSb = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = "AutoLot",
+                DataSource =  ".",
+                IntegratedSecurity = true,
+                ConnectTimeout = 15
+            };
             using (var sql = new SqlConnection())
             {
                 //Символ . указывает, что используется локальная машина
-                sql.ConnectionString = @"Data Source = .; Initial Catalog = AutoLot; Integrated Security = True; Connect Timeout = 30";
+                sql.ConnectionString = sqlConnectionSb.ConnectionString;
                 sql.Open();
+                ShowConnectionStatus(sql);
                 var sqlCommand = new SqlCommand("select * from Inventory", sql);
                 using (var dataReader = sqlCommand.ExecuteReader())
                 {
@@ -28,6 +37,16 @@ namespace _03_AutoLotDataReader
             }
             Console.WriteLine("\n************Работа приложения завершена************");
             Console.ReadLine();
+        }
+
+        private static void ShowConnectionStatus(DbConnection db)
+        {
+            Console.WriteLine("Информация о подключении");
+            Console.WriteLine($"Расположение БД: {db.DataSource}");
+            Console.WriteLine($"Имя БД: {db.Database}");
+            Console.WriteLine($"Версия сервера: {db.ServerVersion}");
+            Console.WriteLine($"Таймаут: {db.ConnectionTimeout}");
+            Console.WriteLine($"Состояние: {db.State}");
         }
     }
 }
