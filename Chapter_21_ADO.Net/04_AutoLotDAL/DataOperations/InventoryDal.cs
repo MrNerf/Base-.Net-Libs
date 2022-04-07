acb30d5558f0e9d86e76bb50de397673fb4fb9e9
Viacheslav <evteevne.ss@gmail.com>
2022-04-07 23:38:10 +0300

    Начат проект создания библиотеки для работы с БД

---
+++ Chapter_21_ADO.Net/04_AutoLotDAL/DataOperations/InventoryDal.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using _04_AutoLotDAL.Models;
+
+namespace _04_AutoLotDAL.DataOperations
+{
+    public class InventoryDal : IDisposable
+    {
+        #region Fields
+
+        private readonly string _connectionString;
+
+        private SqlConnection _sqlConnection;
+
+        #endregion
+
+        #region Ctors
+
+        public InventoryDal() : this(@"Data Source = DRAGON; Initial Catalog = AutoLot; Integrated Security = True") { }
+
+
+        public InventoryDal(string connectionString) => _connectionString = connectionString;
+
+        #endregion
+
+        #region Methods
+
+        public void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection(_connectionString);
+            _sqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (_sqlConnection?.State != ConnectionState.Closed)
+            {
+                _sqlConnection?.Close();
+            }
+            
+        }
+
+        public List<Car> GetInventoryCars()
+        {
+            OpenConnection();
+            var inventoryCar = new List<Car>();
+            using (var sqlDataCommand = new SqlCommand("SELECT * FROM Inventory", _sqlConnection))
+            {
+                sqlDataCommand.CommandType = CommandType.Text;
+                using (var sqlDataReader = sqlDataCommand.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        inventoryCar.Add( new Car()
+                        {
+                            CarId = (int) sqlDataReader["CarId"],
+                            Color = (string)sqlDataReader["Color"],
+                            Mark = (string)sqlDataReader["Mark"],
+                            PetName = (string)sqlDataReader["PetName"]
+                        });
+                    }
+                }
+            }
+
+            return inventoryCar;
+        }
+
+        public Car GetOneCars(int carId)
+        {
+            OpenConnection();
+            var car = new Car();
+            using (var sqlDataCommand = new SqlCommand($"SELECT * FROM Inventory WHERE CarId = {carId}", _sqlConnection))
+            {
+                sqlDataCommand.CommandType = CommandType.Text;
+                using (var sqlDataReader = sqlDataCommand.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        car.CarId = (int) sqlDataReader["CarId"];
+                        car.Color = (string) sqlDataReader["Color"];
+                        car.Mark = (string) sqlDataReader["Mark"];
+                        car.PetName = (string) sqlDataReader["PetName"];
+                    }
+                }
+            }
+
+            return car;
+        }
+
+        public int InsertAuto(string color, string mark, string petName)
+        {
+            OpenConnection();
+            int executeResult;
+            using (var sqlCommend = new SqlCommand($"INSERT INTO Inventory (Mark, Color, PetName) VALUES ('{mark}', '{color}', '{petName}')", _sqlConnection))
+            {
+                sqlCommend.CommandType = CommandType.Text;
+                executeResult = sqlCommend.ExecuteNonQuery();
+            }
+
+            CloseConnection();
+            return executeResult;
+        }
+
+        public int InsertAuto(Car car)
+        {
+            OpenConnection();
+            int executeResult;
+            using (var sqlCommend = new SqlCommand($"INSERT INTO Inventory (Mark, Color, PetName) VALUES ('{car.Mark}', '{car.Color}', '{car.PetName}')", _sqlConnection))
+            {
+                sqlCommend.CommandType = CommandType.Text;
+                executeResult = sqlCommend.ExecuteNonQuery();
+            }
+
+            CloseConnection();
+            return executeResult;
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            CloseConnection();
+        }
+    }
+}
---
+++ Chapter_21_ADO.Net/04_AutoLotDAL/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_AutoLotDAL.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Mark { get; set; }
+        public string Color { get; set; }
+        public string PetName { get; set; }
+    }
+}
